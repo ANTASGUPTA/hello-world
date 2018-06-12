8f4421458250db068e631ea9d61b63c7e3d408e8
ANTASGUPTA <35564545+ANTASGUPTA@users.noreply.github.com>
2018-06-12 23:36:53 +0530

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      DIRTERM
+#
+# Created:     12/06/2018
+# Copyright:   (c) DIRTERM 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+print("Hello! World.")
